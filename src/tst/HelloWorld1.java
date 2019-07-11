d11b731722fcf34d425010e12c807f57a20ba366
Yevhen_Pavlenko <sl1mvsshady@gmail.com>
2019-07-11 13:26:50 +0300

    lol2

--- src/tst/HelloWorld1.java
+++ src/tst/HelloWorld1.java
@@ -4,5 +4,6 @@ public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("lol1");
         System.out.println("lolik");
+        System.out.println("lol2");
     }
 }
